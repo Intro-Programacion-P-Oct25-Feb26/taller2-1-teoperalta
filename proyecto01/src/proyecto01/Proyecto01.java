8bc13f043598f961227a92514be741e8b5b4ba35
teoperalta <teo.peralta.2007@gmail.com>
2025-10-16 12:47:46 -0500

    primer commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -14,6 +14,7 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("San Cayetano Alto, Calle Marcelino Champagnat s/n, Loja")
         // TODO code application logic here
     }
     
